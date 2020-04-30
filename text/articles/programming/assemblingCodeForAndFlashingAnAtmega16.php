134331d1891c9d6bd46e372de2117e9640fb2463
Samuel Brown <ntpower7000@gmail.com>
2020-04-30 22:31:51 +1000

    We have added the skeleton of our next article and we have also added a toDo file

---
+++ text/articles/programming/assemblingCodeForAndFlashingAnAtmega16.php
@@ -0,0 +1,38 @@
+<?php
+  require_once("./common/tools.php");
+  top_module_and_left_side_nav("Assembling Code for and Flashing an Atmega16", "null", true, "null");
+?>
+
+<main class="inset_shadow scroll_style">
+  <div id="position_main_part_of_main">
+    <article id="main_tag_in_main_min_height">
+      <?php
+	heading("ATMEGA16");
+      ?>
+      <br><br><h4>Assembling Code for and Flashing an Atmega16 </h4>
+      <p>
+      </p>
+      <strong>References:</strong>
+      <ul>
+	<li><a href="https://www.instructables.com/id/Command-Line-Assembly-Language-Programming-for-Ard/" target="_blank">https://www.instructables.com/id/Command-Line-Assembly-Language-Programming-for-Ard/</a></li>
+	<li><a href="https://github.com/samcdc6600/Atmega16a-avra-asm-stuff.git" target="_blank">https://github.com/samcdc6600/Atmega16a-avra-asm-stuff.git</a></li>
+	<li><a href="https://github.com/DarkSector/AVR/tree/master/asm/include" target="_blank">https://github.com/DarkSector/AVR/tree/master/asm/include</a></li>
+	<li><a href="https://avrlogic.blogspot.com/2014/11/programming-atmega16-led-blink.html" target="_blank">https://avrlogic.blogspot.com/2014/11/programming-atmega16-led-blink.html</a></li>
+	<li><a href="https://artofcircuits.com/product/usbasp-v2-0-programmer-for-atmel-microcontrollers" target="_blank">https://artofcircuits.com/product/usbasp-v2-0-programmer-for-atmel-microcontrollers</a></li>
+	<li><a href="https://www.theengineeringprojects.com/2018/06/introduction-to-atmega16.html" target="_blank">https://www.theengineeringprojects.com/2018/06/introduction-to-atmega16.html</a></li>
+	<li><a href="https://www.avrfreaks.net/forum/problem-avra-avr" target="_blank">https://www.avrfreaks.net/forum/problem-avra-avr</a></li>
+      </ul>
+    </article>
+
+    <?php
+      topOfPageButton();
+    ?>
+  </div>
+</main>
+
+<?php
+  bottom_module_and_right_side("whyDoesntThisSiteUseJS.html", true, true, "programming", "programmingArticles.html");
+?>
+
+</body>
+</html>
